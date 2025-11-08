564be11fb2f40d4e1ff13968b936f4c60fb7a180
indra dwi wijaya <indradwiwijaya04@gmail.com>
2025-11-08 19:26:28 +0700

    Tambah con_db.php: konfigurasi koneksi database

---
+++ con_db.php
@@ -0,0 +1,11 @@
+<?php
+$host     = "localhost"; // server database
+$user     = "root";      // username 
+$password = "Kamikaze@45";          // password 
+$dbname   = "koneksi_database"; // ubah sesuai database 
+
+// membuat koneksi
+$koneksi = mysqli_connect($host, $user, $password, $dbname);
+
+
+?>
