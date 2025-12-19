0ce05098ba1465bb44b5f4f97ec4c8150f622e3c
Kamikaze45 <indradwiwijaya04@gmail.com>
2025-12-19 11:52:48 +0700

    Update con_dbTest.php

--- con_dbTest.php
+++ con_dbTest.php
@@ -1,11 +1,11 @@
 <?php
-$host     = "localhost"; // server database
-$user     = "root";      // username 
-$password = "Kamikaze@45";          // password 
-$dbname   = "koneksi_database"; // ubah sesuai database 
 
-// membuat koneksi
-$koneksi = mysqli_connect($host, $user, $password, $dbname);
+use PHPUnit\Framework\TestCase;
 
-
-?>
+class con_dbTest extends TestCase
+{
+    public function testKoneksi()
+    {
+        $this->assertTrue(true);
+    }
+}
